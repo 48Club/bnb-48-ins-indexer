282be3cd92e68331dd8a8ef0ab486e2f68b6d1f5
干净 <54979375+du5@users.noreply.github.com>
2023-12-18 02:52:05 +0800

    update api
    	modified:   controler/account.go
    	modified:   dao/account_wallet.go
    	modified:   service/account.go

--- controler/account.go
+++ controler/account.go
@@ -1,10 +1,12 @@
 package controler
 
 import (
+	"bnb-48-ins-indexer/dao"
 	bnb48types "bnb-48-ins-indexer/pkg/types"
 	"bnb-48-ins-indexer/pkg/utils"
 	"bnb-48-ins-indexer/service"
 
+	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/gin-gonic/gin"
 )
 
@@ -47,5 +49,42 @@ func (c *AccountController) Balance(ctx *gin.Context) {
 		return
 	}
 
+	if c.checkBalance(req, res) != nil {
+		utils.FailResponse(ctx, err.Error())
+		return
+	}
+
 	utils.SuccessResponse(ctx, res)
+}
+
+func (c *AccountController) checkBalance(req bnb48types.AccountBalanceReq, res *bnb48types.AccountBalanceRsp) error {
+	var (
+		tickHashs    = mapset.NewSet[string]()
+		inss         = []*dao.InscriptionModel{}
+		insTickHashs = []string{}
+	)
+	for _, v := range req.TickHash {
+		tickHashs.Add(v)
+	}
+	if tickHashs.Cardinality() != len(req.TickHash) {
+		for _, v := range req.TickHash {
+			if tickHashs.Contains(v) {
+				continue
+			}
+			insTickHashs = append(insTickHashs, v)
+		}
+		c.accountS.GetInscription(insTickHashs, &inss)
+		for _, v := range inss {
+			res.Wallet = append(res.Wallet, &dao.AccountWalletModel{
+				Address:  req.Address,
+				Tick:     v.Tick,
+				TickHash: v.TickHash,
+				Balance:  "0",
+				Decimals: v.Decimals,
+			})
+		}
+
+	}
+
+	return nil
 }
--- service/account.go
+++ service/account.go
@@ -85,3 +85,7 @@ func (s *AccountService) List(req bnb48types.ListAccountWalletReq) (*bnb48types.
 	}
 	return resp, nil
 }
+
+func (s *AccountService) GetInscription(insTickHashs []string, inss *[]*dao.InscriptionModel) {
+	_ = database.Mysql().Table("inscription").Where("tick_hash in ?", insTickHashs).Find(&inss)
+}
