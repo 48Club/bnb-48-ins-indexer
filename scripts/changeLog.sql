42dc096c73d5025409eb959ea6e1c0032ec6ae95
干净 <54979375+du5@users.noreply.github.com>
2023-12-18 17:25:40 +0800

    update sql (#27)
    * update sql
    	modified:   scripts/changeLog.sql
    	modified:   scripts/sql/docker-entrypoint-initdb.d/init.sql
    
    * modified:   scripts/sql/docker-entrypoint-initdb.d/init.sql

--- scripts/changeLog.sql
+++ scripts/changeLog.sql
@@ -1,4 +1,4 @@
--- 2023-12-18
+-- 2023-12-18 commit: 21356038c43c84129bb2c2e5038cff172617a9e0
 alter table account_records drop index tx_hash;
-alter table account_records add column op_index int unsigned not null default 0;
+alter table account_records add column op_index int unsigned not null default 0 after `tx_index`;
 alter table account_records add unique tx_hash_op_index (`tx_hash`,`op_index`);
