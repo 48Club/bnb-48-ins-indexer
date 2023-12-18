21356038c43c84129bb2c2e5038cff172617a9e0
asherascout <asherascout@gmail.com>
2023-12-18 17:12:14 +0800

    Feat: 预支持单笔批量 (#26)
    * add columns
    
    * add api:account/balance
    
    * add api:account/balance
    
    * update doc
    
    * update doc
    
    * alter table sql
    
    * alter table sql

---
+++ scripts/changeLog.sql
@@ -0,0 +1,4 @@
+-- 2023-12-18
+alter table account_records drop index tx_hash;
+alter table account_records add column op_index int unsigned not null default 0;
+alter table account_records add unique tx_hash_op_index (`tx_hash`,`op_index`);
