746721b44a619520f2570c2b56557ff412d48291
干净 <54979375+du5@users.noreply.github.com>
2023-12-20 23:09:55 +0800

    fix

--- service/bsc_scan.go
+++ service/bsc_scan.go
@@ -581,11 +581,15 @@ func (s *BscScanService) updateRam(record *dao.AccountRecordsModel, block *types
 	s.pendingTxs.TxsHash.Add(record.TxHash)
 
 	if _, ok := s.pendingTxs.TxsByAddr[record.From]; !ok {
-		s.pendingTxs.TxsByAddr[record.From] = make(map[string]types2.RecordsModelByTxHash)
+		s.pendingTxs.TxsByAddr[record.From] = map[string]types2.RecordsModelByTxHash{
+			record.TickHash: {},
+		}
 	}
 	s.pendingTxs.TxsByAddr[record.From][record.TickHash][record.TxHash] = record
 	if _, ok := s.pendingTxs.TxsByAddr[record.To]; !ok {
-		s.pendingTxs.TxsByAddr[record.To] = make(map[string]types2.RecordsModelByTxHash)
+		s.pendingTxs.TxsByAddr[record.To] = map[string]types2.RecordsModelByTxHash{
+			record.TickHash: {},
+		}
 	}
 	s.pendingTxs.TxsByAddr[record.To][record.TickHash][record.TxHash] = record
 
