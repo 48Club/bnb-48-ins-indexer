3058711e3b0ac5a4c32e3c18d066f68920c63bab
干净 <54979375+du5@users.noreply.github.com>
2024-01-01 21:58:53 +0800

    fix txInput type

--- scripts/changeLog.sql
+++ scripts/changeLog.sql
@@ -2,3 +2,7 @@
 alter table account_records drop index tx_hash;
 alter table account_records add column op_index int unsigned not null default 0 after `tx_index`;
 alter table account_records add unique tx_hash_op_index (`tx_hash`,`op_index`);
+
+-- 2024-01-01
+
+ALTER TABLE `account_records` CHANGE `input` `input` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL;
