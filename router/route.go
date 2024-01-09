6bb1e107f0f4ec64674be4be43da803ecf9e0aa5
干净 <54979375+du5@users.noreply.github.com>
2024-01-09 18:18:01 +0800

    gin: add cache

--- router/route.go
+++ router/route.go
@@ -4,14 +4,22 @@ import (
 	"bnb-48-ins-indexer/config"
 	"bnb-48-ins-indexer/controler"
 	"bnb-48-ins-indexer/pkg/types"
+	"time"
 
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	BSCPerBlockTime = 3 * time.Second
 )
 
 func NewBotRoute(pendingTxs *types.GlobalVariable) *gin.Engine {
 	r := gin.Default()
 	conf := config.GetConfig()
 	bnb48 := r.Group(conf.App.RoutePrefix)
+	store := persistence.NewInMemoryStore(BSCPerBlockTime)
 
 	var (
 		account     = controler.NewAccountController(pendingTxs)
@@ -21,11 +29,11 @@ func NewBotRoute(pendingTxs *types.GlobalVariable) *gin.Engine {
 
 	v1 := bnb48.Group("/v1")
 	{
-		v1.POST("/balance/list", account.List)
-		v1.POST("/record", record.Get)
-		v1.POST("/record/list", record.List)
-		v1.POST("/inscription/list", inscription.List)
-		v1.POST("/account/balance", account.Balance)
+		v1.POST("/balance/list", cache.CachePageAtomic(store, BSCPerBlockTime, account.List))
+		v1.POST("/record", cache.CachePageAtomic(store, BSCPerBlockTime, record.Get))
+		v1.POST("/record/list", cache.CachePageAtomic(store, BSCPerBlockTime, record.List))
+		v1.POST("/inscription/list", cache.CachePageAtomic(store, BSCPerBlockTime, inscription.List))
+		v1.POST("/account/balance", cache.CachePageAtomic(store, BSCPerBlockTime, account.Balance))
 	}
 
 	return r
